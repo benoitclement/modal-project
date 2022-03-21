822a512559ee0e8612bee8e1d5c21dacc1c930b6
Benoît <benoit.clement@si-4you.com>
2022-03-21 04:35:41 +0100

    vue modal component test. scaffolded with vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,58 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>{{ this.title }}</h1>
+  <div v-if="showModal">
+    <Modal @close="toggleModal">
+      <template v-slot:header-button>
+        <a @click="toggleModal">X</a>
+      </template>
+      <h1>Modal title</h1>
+      <p>Content, this is a lot of content. Lines and stuff for my modal.</p>
+      <template v-slot:footer-content>
+        <p>Big promo on maxi best of menus in all Moscow's MacDonalds</p>
+      </template>
+    </Modal>
+  </div>
+  <div v-if="showModal2">
+    <Modal @close="toggleModal2">
+      <template v-slot:header-button>
+        <a @click="alertPlus">+</a>
+      </template>
+      <h1>Second modal</h1>
+      <p>Second / Second / Second</p>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Show modal</button>
+  <button @click="toggleModal2">Show second modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Modal },
+  data() {
+    return {
+      title: "This is Vue !",
+      showModal: false,
+      showModal2: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2;
+    },
+    alertPlus() {
+      window.alert('This is an alert');
+    }
+  },
+
+
 }
 </script>
 
---
+++ src/components/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+  <div @click.self="closeModal" class="backdrop">
+    <div class="modal">
+      <div class="modal-header">
+        <slot name="header-button"></slot>
+      </div>
+      <slot></slot>
+      <div class="modal-footer">
+        <slot name="footer-content">This is default content text for footer</slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    header: {
+      type: String
+    },
+    text: {
+      type: String
+    }
+  },
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    } 
+  }
+}
+</script>
+
+<style>
+  .modal {
+      width: 400px;
+      padding: 20px;
+      margin: 100px auto;
+      background: white;
+      border-radius: 10px;
+    }
+  .modal-header {
+    text-align: right;
+  }
+  .modal-header a {
+      cursor: pointer;
+    }
+  .backdrop {
+      top: 0;
+      position: fixed;
+      background: rgba(0,0,0,0.5);
+      width: 100%;
+      height: 100%;
+    }
+</style>
